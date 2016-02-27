4e92db0223e6be788e689ab3ebd705c9936aea94
DmitryKuk <d1021976@yandex.ru>
2016-02-28 00:43:17 +0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 // Author: Dmitry Kukovinets (d1021976@gmail.com), 27.02.2016, 14:16
 
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <atomic>
 
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+
+
+unsigned long long
+white_pixels(const cv::Mat &mat)
+{
+	std::atomic<unsigned long long> n{0};
+	
+	mat.forEach<uint8_t>(
+		[&](uint8_t pixel, const int * /* unused */)
+		{
+			if (pixel)
+				++n;
+		}
+	);
+	
+	return n;
+}
 
 
 int
@@ -24,15 +43,16 @@ main(int argc, char **argv)
 		return 2;
 	}
 	
-	int
-		frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT),
-		trackbar_frame = 0;
+	int frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT);
 	
 	
+#ifdef INTERACTIVE
 	const std::string
 		original_window	= "original_window",
 		edge_window		= "edge_window",
 		trackbar		= "Frame";
+	
+	int trackbar_frame = 0;
 	
 	
 	cv::namedWindow(original_window);
@@ -41,48 +61,80 @@ main(int argc, char **argv)
 	
 	cv::namedWindow(edge_window);
 	
+	std::cout << "Press space to play/pause, 'n' to next frame, 'b' to previous frame." << std::endl;
 	
-	std::cout
-		<< "Frames count: " << frame_count << std::endl
-		<< "Press space to plau/pause, 'n' to next frame, 'b' to previous frame." << std::endl;
-	
-	
-	cv::Mat mat, edge_mat;
 	int delay = 0;
+#endif	// ifdef INTERACTIVE
+	
+	
+	std::cout << "Frames count: " << frame_count << std::endl;
+	
+	
+	cv::Mat mat, edge_mat, gauss_edge_mat;
 	
 	while (true) {
 		cap >> mat;
 		
-		if (mat.size[0] <= 0) {	// Start again, if end of file reached
+		if (mat.size[0] <= 0) {	// End of file reached
 			cap.set(cv::CAP_PROP_POS_FRAMES, 0);
+#ifdef INTERACTIVE
 			cv::setTrackbarPos(trackbar, original_window, 0);
-			continue;
+			continue;	// Start again in interactive mode
+#else	// ifdef INTERACTIVE
+			break;		// Stop in non-interactive mode
+#endif	// ifdef INTERACTIVE
 		}
 		
 		
 		int frame = cap.get(cv::CAP_PROP_POS_FRAMES);
+		
+#ifdef INTERACTIVE
 		if (trackbar_frame + 1 != frame) {	// Trackbar navigation
 			cap.set(cv::CAP_PROP_POS_FRAMES, trackbar_frame);
 			continue;
 		}
+#endif	// ifdef INTERACTIVE
 		
 		
+#ifdef INTERACTIVE
 		cv::setTrackbarPos(trackbar, original_window, frame);	// Keep trackbar actual
 		
-		
 		double time = cap.get(cv::CAP_PROP_POS_MSEC);
-		
-		
 		cv::setWindowTitle(original_window,
 						   "Frame: " + std::to_string(frame) + ",  Time: " + std::to_string(time) + "ms");
+		
 		cv::imshow(original_window, mat);
+#endif	// ifdef INTERACTIVE
 		
 		
 		cv::Canny(mat, edge_mat, 50, 50);
 		
+		cv::cvtColor(mat, gauss_edge_mat, cv::COLOR_BGR2GRAY);
+		cv::GaussianBlur(gauss_edge_mat, gauss_edge_mat, cv::Size(9, 9), 2, 2);
+		cv::Canny(gauss_edge_mat, gauss_edge_mat, 50, 50);
+		
+		
+		double k = static_cast<double>(100) / (mat.size[0] * mat.size[1]);
+		auto wp = white_pixels(edge_mat),
+			 gauss_wp = white_pixels(gauss_edge_mat);
+		
+		std::cout
+			<< std::setw(6) << frame << ":    "
+			<< std::setw(6) << wp << ":  "
+			<< std::setw(8) << std::fixed << wp * k << "%;    Gauss:  "
+			<< std::setw(6) << gauss_wp << ":  "
+			<< std::setw(8) << std::fixed << gauss_wp * k << '%'
+			<< std::endl;
+		
+		
+#ifdef INTERACTIVE
+		cv::setWindowTitle(edge_window,
+						   "White pixels: " + std::to_string(wp * k) + "; Gauss: " + std::to_string(gauss_wp * k));
 		cv::imshow(edge_window, edge_mat);
+#endif	// ifdef INTERACTIVE
 		
 		
+#ifdef INTERACTIVE
 		// Primitive player
 		auto key = cv::waitKey(delay);
 		if (key >= 0) {
@@ -98,9 +150,13 @@ main(int argc, char **argv)
 					break;
 			}
 		}
+#endif	// ifdef INTERACTIVE
 	}
 	
 	
+#ifdef INTERACTIVE
 	cv::destroyAllWindows();
+#endif	// ifdef INTERACTIVE
+	
 	return 0;
 }
