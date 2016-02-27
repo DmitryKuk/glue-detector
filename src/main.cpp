f10d59739ef0ac2ab7c531212c491a28bcdfec53
DmitryKuk <d1021976@yandex.ru>
2016-02-27 20:34:55 +0300

    Trackbar navigation added. Experiments with Canny in progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <string>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/videoio/videoio.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 
 int
@@ -23,24 +24,64 @@ main(int argc, char **argv)
 		return 2;
 	}
 	
-	cv::namedWindow("Test window");
+	int
+		frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT),
+		trackbar_frame = 0;
+	
+	
+	const std::string
+		original_window	= "original_window",
+		edge_window		= "edge_window",
+		trackbar		= "Frame";
+	
+	
+	cv::namedWindow(original_window);
+	cv::createTrackbar(trackbar, original_window,
+					   &trackbar_frame, frame_count);
+	
+	cv::namedWindow(edge_window);
+	
 	
 	std::cout
-		<< "Frames count: " << cap.get(cv::CAP_PROP_FRAME_COUNT) << std::endl
+		<< "Frames count: " << frame_count << std::endl
 		<< "Press space to plau/pause, 'n' to next frame, 'b' to previous frame." << std::endl;
 	
-	cv::Mat mat;
-	cap >> mat;
 	
+	cv::Mat mat, edge_mat;
 	int delay = 0;
-	while (mat.size[0] > 0) {
-		unsigned long frame = cap.get(cv::CAP_PROP_POS_FRAMES);
+	
+	while (true) {
+		cap >> mat;
+		
+		if (mat.size[0] <= 0) {	// Start again, if end of file reached
+			cap.set(cv::CAP_PROP_POS_FRAMES, 0);
+			cv::setTrackbarPos(trackbar, original_window, 0);
+			continue;
+		}
+		
+		
+		int frame = cap.get(cv::CAP_PROP_POS_FRAMES);
+		if (trackbar_frame + 1 != frame) {	// Trackbar navigation
+			cap.set(cv::CAP_PROP_POS_FRAMES, trackbar_frame);
+			continue;
+		}
+		
+		
+		cv::setTrackbarPos(trackbar, original_window, frame);	// Keep trackbar actual
+		
+		
 		double time = cap.get(cv::CAP_PROP_POS_MSEC);
 		
-		cv::setWindowTitle("Test window",
-						   "Frame: " + std::to_string(frame) + ",  Time: " + std::to_string(time) + "ms");
 		
-		cv::imshow("Test window", mat);
+		cv::setWindowTitle(original_window,
+						   "Frame: " + std::to_string(frame) + ",  Time: " + std::to_string(time) + "ms");
+		cv::imshow(original_window, mat);
+		
+		
+		cv::Canny(mat, edge_mat, 50, 50);
+		
+		cv::imshow(edge_window, edge_mat);
+		
 		
 		// Primitive player
 		auto key = cv::waitKey(delay);
@@ -53,11 +94,10 @@ main(int argc, char **argv)
 					break;
 				case 'b':	// Previous
 					cap.set(cv::CAP_PROP_POS_FRAMES, frame - 2);
+					cv::setTrackbarPos(trackbar, original_window, frame - 2);
 					break;
 			}
 		}
-		
-		cap >> mat;
 	}
 	
 	
